562a845f5529e05d894d0e8a3549979cce027446
Geovanni Santos <117683781+Geovannydynoo@users.noreply.github.com>
2023-09-19 22:26:33 -0300

    Alteração do JSON

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,12 +1,13 @@
 import styles from './Banner.module.css';
 import circuloColorido from 'assets/circulo_colorido.png';
+import minhaFoto from "assets/minha-foto.jpeg"
 
 export default function Banner() {
     return (
         <div className={styles.banner}>
             <div className={styles.apresentacao}>
-                <h1 className={styles.titulo}>Olá, Dev!</h1>
-                <p className="paragrafo">Boas vindas ao meu espaço pessoal! Eu sou Geovanni Santos, Desenvolvedor Full Stack. ;)</p>
+                <h1 className={styles.titulo}>Olá!</h1>
+                <p className="paragrafo">Boas vindas ao meu espaço pessoal! Eu sou Geovania Almeida, CEO do Studio Geo. ;)</p>
             </div>
             <div className={styles.imagens}>
 
@@ -17,7 +18,7 @@ export default function Banner() {
                 />
 
                 <img className={styles.minhaFoto}
-                    src="https://github.com/geovannifsantos.png"
+                    src={minhaFoto}
                     alt="Foto do criador da pagina"
                 />
 
