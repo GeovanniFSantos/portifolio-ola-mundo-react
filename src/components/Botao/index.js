2ef595541770da51fdd9b36c34b13c3375bdcf64
Geovanni Santos <117683781+Geovannydynoo@users.noreply.github.com>
2023-10-27 23:06:16 -0300

    colocando botão de envio pelo whatsapp

--- src/components/Botao/index.js
+++ src/components/Botao/index.js
@@ -1,8 +1,21 @@
+import React from 'react'
 import './Botao.css'
+
+const enviarMensagenToWhatsApp = () => {
+
+    const contatoTEL = '5511913400833'
+    const menssage = "Olá! Gostaria de marca um horário: n/${}"
+
+    const formattedPhoneNumber = contatoTEL.replace(/[-()]/g, '');
+
+    const url = `https://wa.me/${formattedPhoneNumber}?text=${encodeURIComponent(menssage)}`
+
+    window.open(url);
+}
 
 const Botao = (props) => {
     return (
-        <button className='botao'>
+        <button className='botao' onClick={enviarMensagenToWhatsApp}>
             {props.children}
         </button>
     )
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,21 +2,27 @@ import Botao from '../Botao'
 import './Formulario.css'
 
 
-const Formulario = ({ aoCadastrar, times }) => {
+
+const Formulario = ({ servico, data, horario }) => {
+
 
     return (
         <section className='formulario-container'>
             <form className='formulario'>
                 <h2>Agende Seu horário</h2>
                 <div className='campo'>
-                    <label >Serviço</label>
-                    <input type='text'></input>
-                    <label >Date</label>
-                    <input type='date'></input>
-                    <label >Horário</label>
-                    <input type='time'></input>
+
+                    <label id='servico' >Serviço</label>
+                    <input type='text' value={servico} placeholder="Digite o tipo de Serviço...."></input>
+
+                    <label id='data'>Date</label>
+                    <input type='date' value={data}></input>
+
+                    <label id='horario'>Horário</label>
+                    <input type='time' value={horario}></input>
+
                 </div>
-                <Botao>
+                <Botao >
                     Agendar
                 </Botao>
             </form>
